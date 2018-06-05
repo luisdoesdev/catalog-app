5764e2f80d8480495e3705c1672f736c36f24e50
EC2 Default User <ec2-user@ip-172-31-72-136.ec2.internal>
2018-06-05 13:24:15 +0000

    sign in and sing out finally working TAHH DAHH

--- run.py
+++ run.py
@@ -7,7 +7,7 @@ from app import app
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
     #Cloud
-    #app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)))
+    app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)))
     #Local
-    app.run(host="0.0.0.0", port=5000)
+    #app.run(host="0.0.0.0", port=5000)
     DEBUG = True
