a5209d729726a45e82d9efd0f6157de842d98c60
Luis <letorruella@gmal.com>
2018-06-11 06:14:47 -0400

    blueprint starts

--- app/__init__.py
+++ app/__init__.py
@@ -6,9 +6,19 @@ from flask import Flask
 
 # Initialize the APP
 app = Flask(__name__)
+app.config.from_pyfile('flask.cfg')
 app.debug = True
+
+
+
 # Loads the routes
 from app import routes
 
+
+## Blueprints
+from app.crud.views import crud_blueprint
+
+## Register the blueprints
+app.register_blueprints(user_blueprint)
 
 
---
+++ app/templates/crud/templates/views.py
@@ -0,0 +1,31 @@
+# project/recipes/views.py
+
+from flask import render_template, Blueprint
+
+crud_blueprint = Blueprint('crud',__name__, template_folder='templates')
+
+
+@crud_blueprint('/add', methods=['GET', 'POST'])
+@login_required
+def add():
+    username = usernameState(state)
+    user = session.query(User).filter_by\
+        (email=login_session['email']).one_or_none()
+    category = session.query(Category).all()
+    if request.method == 'POST':
+        newItem = Item(
+            name=request.form['name'],
+            description=request.form['description'],
+            category_id=request.form['categories'],
+            user_id=user.id
+        )
+
+        session.add(newItem)
+        session.commit()
+        return redirect(url_for('index'))
+    return render_template(
+        'add.html',
+        category=category,
+        username=username,
+        STATE=state)
+
