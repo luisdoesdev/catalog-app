c47dfe602703876b8cad39f1f810dae03661a00b
Luis <letorruella@gmal.com>
2018-06-17 17:35:03 -0400

    hello world created in __init__.py

---
+++ project/__init__.py
@@ -0,0 +1,27 @@
+# __init__.py
+
+
+# IMPORTS
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_modus import Modus
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+modus = Modus(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://localhost/catalog'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = "THIS SHOULD BE HIDDEN!"
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+'''
+# import a blueprint that we will create
+from project.user.views import owners_blueprint
+
+# register our blueprints with the application
+app.register_blueprint(owners_blueprint, url_prefix='/user')
+'''
+@app.route('/')
+def root():
+    return "HELLO BLUEPRINTS!"
