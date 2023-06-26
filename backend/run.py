876978102d45b04a34ae750adfe162f5409ef3a9
Luis E Torruella <28609995+letorruella@users.noreply.github.com>
2023-06-26 17:24:10 -0400

    run test

--- backend/run.py
+++ backend/run.py
@@ -1,13 +1,24 @@
-# run.py
-import os
+from flask import Flask
+app = Flask(__name__)
 
-from project import app
-
+@app.route('/')
+def hello_world():
+    return 'Hello World'
 
 if __name__ == '__main__':
-    app.secret_key = 'super_secret_key'
-    # Cloud
-    app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)))
-    # Local
-    #app.run(host="0.0.0.0", port=5000)
-    DEBUG = True
+    app.run()
+
+
+# run.py
+# import os
+
+# # from project import app
+
+
+# if __name__ == '__main__':
+#     app.secret_key = 'super_secret_key'
+#     # Cloud
+#     app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)))
+#     # Local
+#     #app.run(host="0.0.0.0", port=5000)
+#     DEBUG = True
